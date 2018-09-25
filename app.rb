263bf2786365604c87531d1981b6a074b02df573
alakijayo <alakijaayo@gmail.com>
2018-09-25 10:21:36 +0100

    Viewing Bookmarks

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require 'sinatra/base'
+require './lib/bookmark'
 
-class Bookmark < Sinatra::Base
+class Bookmarks < Sinatra::Base
   get '/' do
+    erb :homepage
+  end
+
+run! if app_file == $0
+
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
     erb :bookmark
   end
+
+
 end
--- spec/features/bookmark_spec.rb
+++
@@ -1,6 +0,0 @@
-feature 'viewing bookmarks' do
-  scenario 'visiting the home page' do
-     visit('/')
-     expect(page).to have_content "Bookmark Manager"
-  end
-end
