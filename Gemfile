9c8fbe44b2b7502a912cf5878ea35bc031ea9ada
James Rodney <jlrodney95@gmail.com>
2018-09-27 14:18:23 +0100

    first commit

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '2.5.1'
 
 gem 'rake'
 gem 'sinatra'
+gem 'sinatra-flash'
 gem 'capybara'
 gem 'rspec'
 gem 'pg'
--- app.rb
+++ app.rb
@@ -1,15 +1,19 @@
 require 'sinatra/base'
+require 'sinatra/flash'
 require './lib/bookmark'
 
 class Bookmarks < Sinatra::Base
+  enable :sessions
+  register Sinatra::Flash
+
   get '/' do
+    flash[:notice] = "Hooray, flash is working!"
     erb :homepage
   end
 
 run! if app_file == $0
 
   get '/bookmarks' do
-    p ENV
     @bookmarks = Bookmark.all
     erb :bookmark
   end
